a49352b67a8665a2ea5e1bc81fc5acc2617caa41
dddiehn <dddiehn@gmail.com>
2020-06-22 08:11:40 -0500

    basic math controller

---
+++ rest-with-springboot-udemy/src/main/java/br/com/erudio/request/converters/NumberConverter.java
@@ -0,0 +1,29 @@
+package br.com.erudio.request.converters;
+
+import br.com.erudio.exception.UnsupportedMathOperationException;
+
+public class NumberConverter {
+	public static Double convertToDouble(String strNumber) {
+		if (strNumber == null) return 0D;
+		String number = strNumber.replaceAll(",", ".");
+		if (isNumeric(number)) {
+			return Double.parseDouble(number);
+		} else {
+			return 0D;
+		}
+	}
+
+	public static boolean isNumeric(String strNumber) {
+		if (strNumber == null) return false;
+		String number = strNumber.replaceAll(",", ".");
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+	}
+
+	public static void numericArguments(String[] numbers) {
+		for (String number : numbers) {
+			if (!isNumeric(number)) {
+				throw new UnsupportedMathOperationException("Please set a numeric value!");
+			}
+		}
+	}
+}
